0fd6a42ae06576c53945d263c2bfe0d2dbab19a3
sgao <gao33.bnl@gmail.com>
2023-06-11 11:20:14 -0400

    update

--- DUNE_DAT_FPGA_V2B/SRC/Version_Reg.vhd
+++ DUNE_DAT_FPGA_V2B/SRC/Version_Reg.vhd
@@ -9,7 +9,7 @@ ENTITY version_reg IS
 END version_reg;
 ARCHITECTURE rtl OF version_reg IS
 BEGIN
-    data_out <= X"23A";
-    Date_s <= X"20230512";
-    Time_s <= X"182018";     
+    data_out <= X"241";
+    Date_s <= X"20230611";
+    Time_s <= X"095129";     
 END rtl;
