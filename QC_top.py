bc232a16d41ac5df2b0d1f06343f0835ec730d9b
linyun_BNL <lingyun.lke@gmail.com>
2023-12-12 20:49:18 -0500

    wib_util.so
    the 29k without a poke function

--- QC_top.py
+++ QC_top.py
@@ -6,7 +6,7 @@ import time
 ag = argparse.ArgumentParser()
 ag.add_argument("fembs", help="a list of femb slots number", type=int, nargs='+')
 #ag.add_argument("-s", "--save", help="number of pulses to be saved", type=int, default=1)
-ag.add_argument("-t", "--task", help="which QC tasks to be performed", type=int, choices=range(1,16),  nargs='+', default=range(1,16))
+ag.add_argument("-t", "--task", help="which QC tasks to be performed", type=int, choices=range(1,16+1),  nargs='+', default=range(1,16+1))
 args = ag.parse_args()
 
 fembs = args.fembs
@@ -18,9 +18,9 @@ qc=QC_Runs(fembs)
 qc.pwr_fembs('on')
 
 tt={}
-
+t1=time.time()
 for tm in tasks:
-    t1=time.time()
+
     print("start tm=",tm)
     if tm==1:
        qc.pwr_consumption()
@@ -76,9 +76,9 @@ for tm in tasks:
     if tm == 16:
        qc.femb_test_pattern_pll()
 
-    t2=time.time()
-    tt[tm]=t2-t1
-    time.sleep(1)
+t2=time.time()
+tt[tm]=t2-t1
+time.sleep(1)
 
 qc.pwr_fembs('off')
 print(tt)
