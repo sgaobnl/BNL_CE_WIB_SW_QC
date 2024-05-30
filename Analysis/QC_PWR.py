6e3c33e65a16dc37083e9315c720bba7bfcc3e05
fanrado <radofana@gmail.com>
2024-05-30 16:35:59 -0400

    Update on the FE_PWR method -> Change the format of the data to be saved in the output json file -> Updated the plots by summing up the voltage (current or power consumption) in VDDA, VDDO, and VDDP. -> Declared a new method "analyzeRawData" to analyze the channel response during the power consumption measurement of the LArASIC ++ To be done: Implement the selection criteria and update the json data format

--- Analysis/QC_PWR.py
+++ Analysis/QC_PWR.py
@@ -11,6 +11,12 @@ import json, pickle
 from utils import printItem, decodeRawData, createDirs, dumpJson
 
 class QC_PWR:
+    '''
+        output data:
+        {
+            
+        }
+    '''
     def __init__(self, root_path: str, data_dir: str, output_dir: str):
         self.tms = 1
         self.qc_pwr_filename = 'QC_PWR.bin'
@@ -37,8 +43,6 @@ class QC_PWR:
         for ichip in range(8):
             FE_ID = self.logs_dict['FE{}'.format(ichip)]
             data_by_config[FE_ID] = dict()
-            ## ---- NEED TO ORGARNIZE THIS PART
-            ## ---- The parameters in the raw data correspond to different configurations of the LArASIC --> Need datasheet
             for param in pwr_params:
                 data_oneconfig = self.raw_data[param][3]
                 data_by_config[FE_ID][param] = dict()
@@ -48,59 +52,91 @@ class QC_PWR:
                         if keyname=="VPPP":
                             keyname = "VDDP"
                         data_by_config[FE_ID][param][keyname] = data_oneconfig[val]
-        print(data_by_config['002-06204'])
-        print('==='*20)
         pwr_all_chips = dict()
         for ichip in range(8):
-            oneChip_data = {'VDDA': {'900mV': {}, '200mV': {}}, 'VDDO': {'900mV': {}, '200mV': {}}, 'VDDP': {'900mV': {}, '200mV': {}}}
+            # oneChip_data = {'VDDA': {'900mV': {}, '200mV': {}}, 'VDDO': {'900mV': {}, '200mV': {}}, 'VDDP': {'900mV': {}, '200mV': {}}}
+            oneChip_data = {'900mV': {}, '200mV': {}, 'units': ['V', 'mA', 'mW']}
             FE_ID = self.logs_dict['FE{}'.format(ichip)]
             for param in pwr_params:
                 configs = [conf for conf in param.split('_') if conf!='PWR']
+                v_i_pwr = np.array([0,0,0])
+                v_i_pwr_params = ['V', 'I', 'P']
                 for pwr_rail in ['VDDA', 'VDDO', 'VDDP']:
-                    oneChip_data[pwr_rail][param_meanings[configs[2]]]['_'.join([configs[0], configs[1]])] = data_by_config[FE_ID][param][pwr_rail]
+                    v_i_pwr = v_i_pwr + np.array(data_by_config[FE_ID][param][pwr_rail])
+                    # oneChip_data[pwr_rail][param_meanings[configs[2]]]['_'.join([configs[0], configs[1]])] = data_by_config[FE_ID][param][pwr_rail]
+                oneChip_data[param_meanings[configs[2]]]['_'.join([configs[0], configs[1]])] = {v_i_pwr_params[i]: np.around(v_i_pwr[i], 4) for i in range(3)}
             pwr_all_chips[FE_ID] = oneChip_data
-        print(pwr_all_chips['002-06204'])
-        print('==='*20)
         # Making plots
+        plt.rcParams['font.size'] = 13
         Baselines = ['200mV', '900mV']
-        pwr_rails = ['VDDA', 'VDDO', 'VDDP']
-        for pwr_rail in pwr_rails:
-            for ichip, chip_id in enumerate(pwr_all_chips.keys()):
-                fig1, ax1 = plt.subplots()
-                fig2, ax2 = plt.subplots()
-                fig3, ax3 = plt.subplots()
-                for BL in Baselines:
-                    data_to_plot = pwr_all_chips[chip_id][pwr_rail][BL]
-                    configs = list(data_to_plot.keys())
-                    voltage, current, power = [], [], []
-                    xticks_titles = []
-                    for config in configs:
-                        tmp = config.split('_')
-                        tmptmp = '\n'.join([param_meanings[tmp[0]], param_meanings[tmp[1]]])
-                        xticks_titles.append(tmptmp)
-                        voltage.append(data_to_plot[config][0])
-                        current.append(data_to_plot[config][1])
-                        power.append(data_to_plot[config][2])
-                    
-                    # voltage
-                    ax1.plot(voltage, label=BL, marker='.', markersize=12)
-                    ax1.set_xticks([0, 1, 2], xticks_titles)
-                    ax1.set_ylabel('Voltage (V)')
-                    # current
-                    ax2.plot(current, label=BL, marker='.', markersize=12)
-                    ax2.set_xticks([0, 1, 2], xticks_titles)
-                    ax2.set_ylabel('Current (mA)')
+        for ichip, chip_id in enumerate(pwr_all_chips.keys()):
+            fig_v, ax_v = plt.subplots()
+            fig_i, ax_i = plt.subplots()
+            fig_p, ax_p = plt.subplots()
+            for BL in Baselines:
+                data_to_plot = pwr_all_chips[chip_id][BL]
+                # configs = list(data_to_plot.keys())
+                voltage, current, power = [], [], []
+                xticks_labels = []
+                configs = ['SDD0_SDF0', 'SDD0_SDF1', 'SDD1_SDF0']
+                for config in configs:
+                    tmp = config.split('_')
+                    xticks_labels.append('\n'.join([param_meanings[tmp[0]], param_meanings[tmp[1]]]))
+                    voltage.append(data_to_plot[config]['V'])
+                    current.append(data_to_plot[config]['I'])
+                    power.append(data_to_plot[config]['P'])
                 # voltage
-                ax1.set_title('{} - Voltage'.format(pwr_rail))
-                ax1.legend()
-                fig1.savefig('/'.join([self.FE_output_DIRs[ichip], '{}_voltage.png'.format(pwr_rail)]))
+                ax_v.plot(voltage, label=BL, marker='.', markersize=12)
+                ax_v.set_xticks([0, 1, 2], xticks_labels)
                 # current
-                ax2.set_title('{} - Current'.format(pwr_rail))
-                ax2.legend()
-                fig2.savefig('/'.join([self.FE_output_DIRs[ichip], '{}_current.png'.format(pwr_rail)]))
-                plt.close()
-        sys.exit()
+                ax_i.plot(current, label=BL, marker='.', markersize=12)
+                ax_i.set_xticks([0, 1, 2], xticks_labels)
+                # power
+                ax_p.plot(power, label=BL, marker='.', markersize=12)
+                ax_p.set_xticks([0, 1, 2], xticks_labels)
                 
+            # voltage
+            ax_v.set_ylabel('Voltage ({})'.format(pwr_all_chips[chip_id]['units'][0]))
+            ax_v.legend()
+            fig_v.savefig('/'.join([self.FE_output_DIRs[ichip], '{}_Voltage.png'.format(self.qc_pwr_filename.split('.')[0])]))
+            plt.close(fig=fig_v)
+            # current
+            ax_i.set_ylabel('Current ({})'.format(pwr_all_chips[chip_id]['units'][1]))
+            ax_i.legend()
+            fig_i.savefig('/'.join([self.FE_output_DIRs[ichip], '{}_Current.png'.format(self.qc_pwr_filename.split('.')[0])]))
+            plt.close(fig=fig_i)
+            # power
+            ax_p.set_ylabel('Power ({})'.format(pwr_all_chips[chip_id]['units'][2]))
+            ax_p.legend()
+            fig_p.savefig('/'.join([self.FE_output_DIRs[ichip], '{}_PowerConsumption.png'.format(self.qc_pwr_filename.split('.')[0])]))
+            plt.close(fig=fig_p)
+
+        # update data with the link to the plots and save everything in a json file
+        for ichip, chip_id in enumerate(pwr_all_chips.keys()):
+            FE_output_dir = self.FE_output_DIRs[ichip]
+            oneChip_data = {
+                            '200mV': {'V': {}, 'I': {}, 'P': {}, 'units': ['V', 'mA', 'mW']}, 
+                            '900mV': {'V': {}, 'I': {}, 'P': {}, 'units': ['V', 'mA', 'mW']}
+                            }
+            Baselines = ['200mV', '900mV']
+            configs = ['SDD0_SDF0', 'SDD0_SDF1', 'SDD1_SDF0']
+            tmpdata_onechip = pwr_all_chips[chip_id]
+            for BL in Baselines:
+                for config in configs:
+                    oneChip_data[BL]['V'][config] = tmpdata_onechip[BL][config]['V']
+                    oneChip_data[BL]['I'][config] = tmpdata_onechip[BL][config]['I']
+                    oneChip_data[BL]['P'][config] = tmpdata_onechip[BL][config]['P']
+                oneChip_data[BL]['V']['link_to_img'] = '/'.join([FE_output_dir, '{}_Voltage.png'.format(self.qc_pwr_filename.split('.')[0])])
+                oneChip_data[BL]['I']['link_to_img'] = '/'.join([FE_output_dir, '{}_Current.png'.format(self.qc_pwr_filename.split('.')[0])])
+                oneChip_data[BL]['P']['link_to_img'] = '/'.join([FE_output_dir, '{}_PowerConsumption.png'.format(self.qc_pwr_filename.split('.')[0])])
+            dumpJson(output_path=FE_output_dir, output_name="QC_PWR_data", data_to_dump=oneChip_data)
+
+    def analyzeRawData(self):
+        '''
+            For each configuration corresponds a raw data of the channel response. 
+            This method aims to analyze the channel response during the power measuremet
+        '''
+        pass
 
 if __name__ =='__main__':
     root_path = '../../Data_BNL_CE_WIB_SW_QC'
@@ -109,4 +145,3 @@ if __name__ =='__main__':
     for data_dir in list_data_dir:
         qc_pwr = QC_PWR(root_path=root_path, data_dir=data_dir, output_dir=output_path)
         qc_pwr.FE_PWR()
-        sys.exit()
