4438935a2440bcee91ea81bf7101b023962ed829
fanrado <radofana@gmail.com>
2024-06-10 13:04:22 -0400

    - completed the method mon_dac : decodes the DAC Monitoring - added the method decodeFE_MON: combine all other decoding methods, calculate the Gain and INL for each configuration and chip, and save the data in a FE_MON.json file

--- Analysis/QC_FE_MON.py
+++ Analysis/QC_FE_MON.py
@@ -4,8 +4,10 @@
 #   Decode QC_MON.bin
 ############################################################################################
 
+import numpy as np
 import os, sys, pickle
-from utils import printItem, createDirs, dumpJson
+from utils import printItem, createDirs, dumpJson, linear_fit
+import matplotlib.pyplot as plt
 
 class FE_MON:
     def __init__(self, root_path: str, data_dir: str, output_path: str):
@@ -21,6 +23,11 @@ class FE_MON:
         self.logs_dict = self.raw_data['logs']
         createDirs(logs_dict=self.logs_dict, output_dir=output_path)
         self.FE_outputDIRs = ['/'.join([output_path, self.logs_dict['FE{}'.format(ichip)], 'QC_FE_MON']) for ichip in range(8)]
+        for d in self.FE_outputDIRs:
+            try:
+                os.mkdir(d)
+            except OSError:
+                pass
         print(self.mon_params)
 
     def getBaselines(self):
@@ -97,14 +104,91 @@ class FE_MON:
         return out_dict
 
     def mon_dac(self):
+        '''
+            Output:
+             {
+                "chipID0":{
+                    "config0": {
+                        "DAC": [],
+                        "data": []
+                    },
+                    "config1": {
+                        "DAC": [],
+                        "data": []
+                    },
+                    ...
+                },
+                "chipID1":{
+                    "config0": {
+                        "DAC": [],
+                        "data": []
+                    },
+                    "config1": {
+                        "DAC": [],
+                        "data": []
+                    },
+                    ...
+                },
+                ...
+             }
+            where len(DAC) = 64
+        '''
+        tmpout_dict = dict()
         params = [param for param in self.mon_params if 'DAC' in param]
         for param in params:
             data = self.raw_data[param]
+            dac_values = []
+            dacperchip = [[] for _ in range(8)]
             for idac in range(64):
                 dac_val = data[idac][0]
-                chips_data = data[idac][1]
-                
-                sys.exit()
+                chipsperDAC = data[idac][1]
+                dac_values.append(dac_val)
+                for ichip in range(8):
+                    dacperchip[ichip].append(chipsperDAC[ichip])
+            config = '_'.join( [p for p in param.split('_') if p!='MON'] )    
+            tmpout_dict[config] = {"dac_values": dac_values, "all_chipsdata": dacperchip}
+        
+        OUT_dict = {self.logs_dict['FE{}'.format(ichip)]: dict() for ichip in range(8)}
+        for ichip in range(8):
+            FE_ID = self.logs_dict['FE{}'.format(ichip)]
+            for config in tmpout_dict.keys():
+                OUT_dict[FE_ID][config] = {
+                    "DAC": tmpout_dict[config]["dac_values"],
+                    "data": tmpout_dict[config]["all_chipsdata"][ichip]
+                }
+        #-- Code example for making the plot
+        # plt.figure()
+        # d = OUT_dict['002-06204']
+        # for key in d.keys():
+        #     plt.scatter(x=d[key]['DAC'], y=d[key]['data'], label=key)
+        # plt.legend()
+        # plt.show()
+        # plt.close()
+        # sys.exit()
+        return OUT_dict
+    
+    def decodeFE_MON(self):
+        BL = self.getBaselines()
+        vbgr_temp = self.getvgbr_temp(unitOutput='mV')
+        dac_meas = self.mon_dac()
+        for ichip in range(8):
+            FE_ID = self.logs_dict['FE{}'.format(ichip)]
+            dac_meas_chip = dac_meas[FE_ID]
+            # print(dac_meas_chip)
+            # sys.exit()
+            # get Gain and INL
+            for config in dac_meas_chip.keys():
+                GAIN, Yintercept, INL = linear_fit(x=dac_meas_chip[config]['DAC'], y=dac_meas_chip[config]['data'])
+                dac_meas_chip[config]['GAIN'] = np.round(GAIN,4)
+                dac_meas_chip[config]['unit_of_gain'] = 'mV/bit'
+                dac_meas_chip[config]['INL'] = np.round(INL,4)*100
+            oneChipData = {
+                "BL": BL[FE_ID],
+                "VBGR_Temp": vbgr_temp[FE_ID],
+                "DAC_meas": dac_meas_chip
+            }
+
+            dumpJson(output_path=self.FE_outputDIRs[ichip], output_name='FE_MON', data_to_dump=oneChipData, indent=4)
 
 if __name__ == '__main__':
     root_path = '../../Data_BNL_CE_WIB_SW_QC'
@@ -113,5 +197,5 @@ if __name__ == '__main__':
     list_data_dir = [dir for dir in os.listdir(root_path) if '.zip' not in dir]
     for data_dir in list_data_dir:
         fe_Mon = FE_MON(root_path=root_path, data_dir=data_dir, output_path=output_path)
-        fe_Mon.mon_dac()
-        sys.exit()
+        fe_Mon.decodeFE_MON()
+        # sys.exit()
