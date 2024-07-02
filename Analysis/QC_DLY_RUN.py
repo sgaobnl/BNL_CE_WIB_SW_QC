579fc0ea5d8b12e3d7a912097e458c4fca50bb05
fanrado <radofana@gmail.com>
2024-07-02 17:41:31 -0400

    Initial commit - Script to decode the DELAY_RUN data

---
+++ Analysis/QC_DLY_RUN.py
@@ -0,0 +1,195 @@
+############################################################################################
+#   created on 7/2/2024 @ 10:20
+#   email: radofanantenan.razakamiandra@stonybrook.edu
+#   Analyze the calibration data: QC_DLY_RUN.bin
+############################################################################################
+
+import os, sys
+import numpy as np
+from utils import printItem, createDirs, dumpJson, linear_fit, LArASIC_ana, decodeRawData, BaseClass, getMaxAmpIndices
+import matplotlib.pyplot as plt
+
+class QC_DLY_RUN(BaseClass):
+    def __init__(self, root_path: str, data_dir: str, output_path: str, generateWf=False):
+        printItem("DELAY RUN")
+        self.generateWf = generateWf
+        super().__init__(root_path=root_path, data_dir=data_dir, output_path=output_path, tms=7, QC_filename="QC_DLY_RUN.bin", generateWaveForm=self.generateWf)
+        self.suffixName = "DLY_RUN"
+    
+    def getPhase_Period(self):
+        Phase_Period = dict()
+        for param in self.params:
+            data = self.raw_data[param]
+            phase = data[3]
+            period = data[4]
+            Phase_Period[param] = {'phase': phase, 'period': period}
+        # print(Phase_Period)
+        return Phase_Period
+
+    def avgWf(self, data_in: list):
+        newdata = []
+        for ichip in range(8):
+            onechipdata = data_in[ichip]
+            newchipdata = {}
+            width = 500
+            for chn in range(16):
+                newchipdata['CH{}'.format(chn)] = {'wf': [], 'pedestal': 0, 'rms': 0}
+                pedestal = 0
+                rms = 0
+                chdata = np.array(onechipdata[chn])
+                period = 1000
+                newchdata = []
+                num_samples = 3
+                for i in range(num_samples):
+                    istart = i*period
+                    iend = istart + period
+                    chunkdata = chdata[istart : iend]
+                    if i==0:
+                        pedestal = np.mean(chunkdata[:100])
+                        rms = np.std(chunkdata[:100])
+                    # maxpos = int((iend-istart)/2 - 50)
+                    # if dac!='4':
+                        # print(np.max(chunkdata))
+                    maxpos = np.where(chunkdata==np.max(chunkdata))[0][0]
+                    istart = maxpos-10
+                    iend = maxpos+250
+                    if istart < 0:
+                        istart = 0
+                    wf = chunkdata[istart : iend]
+                    if len(wf)!=0:
+                        newchdata.append(list(wf))
+                L = [len(d) for d in newchdata]
+                # print(L)
+                imax = np.where(L==np.median(L))[0][0]
+                if imax!=0:
+                    newchdata[imax].pop(-1)
+                sameLength = False
+                while not sameLength:
+                    pos = [i for i in range(len(L)) if L[i]!=L[imax]]
+
+                    for k in pos:
+                        if k==len(newchdata):
+                            newchdata.pop(-1)
+                        else:
+                            newchdata.pop(k)
+                    LL = [len(d) for d in newchdata]
+                    # print(LL)
+                    if len(LL)==1:
+                        sameLength = True
+                    else:
+                        pos = [i for i in range(len(LL)) if LL[i]!=LL[imax]]
+                        if len(pos)==0:
+                            sameLength = True
+                newchdata = np.array(newchdata)
+                avg_wf = np.average(np.transpose(newchdata), axis=1, keepdims=False)
+                newchipdata['CH{}'.format(chn)]['wf'] = avg_wf
+                newchipdata['CH{}'.format(chn)]['pedestal'] = pedestal
+                newchipdata['CH{}'.format(chn)]['rms'] = rms
+            newdata.append(newchipdata)
+        return newdata
+
+    def decode(self):
+        decodedData = dict()
+        Phase_Period = self.getPhase_Period()
+
+        for param in self.params:
+            __rawdata = self.raw_data[param]
+            fembs = __rawdata[0]
+            rawdata = __rawdata[1]
+            # decodedData = decodeRawData(fembs=fembs, rawdata=rawdata, needTimeStamps=False)
+            data = self.avgWf(data_in=decodeRawData(fembs=fembs, rawdata=rawdata, needTimeStamps=False))
+            decodedData[param] = {'data': data, 'phase': Phase_Period[param]['phase'], 'period': Phase_Period[param]['period']}
+        return decodedData
+    
+    def organizeData(self, decodedData):
+        organized_data = dict()
+        for ichip in range(8):
+            FE_ID = self.logs_dict['FE{}'.format(ichip)]
+            organized_data[FE_ID] = dict()
+            for param in self.params:
+                organized_data[FE_ID][param] = dict()
+                for chn in range(16):
+                    organized_data[FE_ID][param]['CH{}'.format(chn)] = dict()
+        for param in self.params:
+            tmpdata = decodedData[param]
+            phase = tmpdata['phase']
+            period = tmpdata['period']
+            for ichip in range(8):
+                FE_ID = self.logs_dict['FE{}'.format(ichip)]
+                organized_data[FE_ID][param]['phase'] = phase
+                organized_data[FE_ID][param]['period'] = period
+                chipdata = tmpdata['data'][ichip]
+                for chn in range(16):
+                    chdata = chipdata['CH{}'.format(chn)]['wf']
+                    pedestal = chipdata['CH{}'.format(chn)]['pedestal']
+                    rms = chipdata['CH{}'.format(chn)]['rms']
+                    organized_data[FE_ID][param]['CH{}'.format(chn)] = {'data': chdata, 'pedestal': pedestal, 'rms': rms}
+        return organized_data
+    
+    def getAmplitudes(self, organizedData: dict):
+        # logs
+        logs = {
+                "date": self.logs_dict['date'],
+                "testsite": self.logs_dict['testsite'],
+                "env": self.logs_dict['env'],
+                "note": self.logs_dict['note'],
+                "DAT_SN": self.logs_dict['DAT_SN'],
+                "WIB_slot": self.logs_dict['DAT_on_WIB_slot']
+            }
+        for ichip in range(8):
+            FE_ID = self.logs_dict['FE{}'.format(ichip)]
+            tmpchipData = organizedData[FE_ID]
+            chipdata = {
+                'logs': logs
+            }
+            for param in tmpchipData.keys():
+                phase = tmpchipData[param]['phase']
+                period = tmpchipData[param]['period']
+                key = "Phase{}_Period{}".format(phase, period)
+                chipdata[key] = {'phase': phase, 'period': period}
+                for chn in range(16):
+                    wf = tmpchipData[param]['CH{}'.format(chn)]['data']
+                    pedestal = tmpchipData[param]['CH{}'.format(chn)]['pedestal']
+                    rms = np.round(tmpchipData[param]['CH{}'.format(chn)]['rms'], 4)
+                    #
+                    ppeak = np.round(np.max(wf)-pedestal, 4)
+                    npeak = np.round(pedestal - np.min(wf), 4)
+                    chipdata[key]['CH{}'.format(chn)] = {'pospeak': ppeak, 'negpeak': npeak, 'pedestal': pedestal, 'rms': rms}
+            
+            # save data
+            print("Save data for {}...".format(FE_ID))
+            dumpJson(output_path=self.FE_outputDIRs[FE_ID], output_name=self.suffixName, data_to_dump=chipdata, indent=4)
+
+    def plotWaveforms(self, organizedData: dict):
+        for ichip in range(8):
+            FE_ID = self.logs_dict['FE{}'.format(ichip)]
+            chipdata = organizedData[FE_ID]
+            for chn in range(16):
+                CH = 'CH{}'.format(chn)
+                plt.figure(figsize=(15,15))
+                for param in chipdata.keys():
+                    phase = chipdata[param]['phase']
+                    period = chipdata[param]['period']
+                    wf = chipdata[param][CH]['data']
+                    plt.plot(wf, label="phase = {}, period = {}".format(phase, period))
+                plt.legend(loc='upper right')
+                print("save waveform for {}, {}...".format(FE_ID, CH))
+                plt.savefig('/'.join([self.FE_outputPlots_DIRs[FE_ID], '{}_CH{}.png'.format(self.suffixName, chn)]))
+                plt.close()
+
+    def run_DLY_RUN(self):
+        decodedData = self.decode()
+        organizedData = self.organizeData(decodedData=decodedData)
+        self.getAmplitudes(organizedData=organizedData)
+        if self.generateWf:
+            self.plotWaveforms(organizedData=organizedData)
+
+if __name__ == '__main__':
+    root_path = '../../Data_BNL_CE_WIB_SW_QC'
+    output_path = '../../Analyzed_BNL_CE_WIB_SW_QC'
+
+    list_data_dir = [dir for dir in os.listdir(root_path) if '.zip' not in dir]
+    for i, data_dir in enumerate(list_data_dir):
+        dly_run = QC_DLY_RUN(root_path=root_path, data_dir=data_dir, output_path=output_path, generateWf=True)
+        dly_run.run_DLY_RUN()
+        # sys.exit()
