b56b5accc2ced5918aec77cbf8d00572de9ca244
sgaobnl <gao33.bnl@gmail.com>
2025-08-11 10:49:15 -0400

    Update RTS_DAT_rev0.py

--- RTS_DAT_rev0.py
+++ RTS_DAT_rev0.py
@@ -80,8 +80,6 @@ def RTS_debug (info, status=None, trayno=None, trayc=None, trayr=None, sinkno=No
     elif "T2T" in info:
         print ("Chip is moved from Tray to Tray") 
 
-    rts.rts_idle()
-
     while True:
         print ("444-> Shutdown RTS and exit anyway")
         #print ("1->move chip to Tray#1_Col#15_Row#6")
@@ -572,6 +570,7 @@ while (len(duts) > 0) :
             ocr_info = ocr_chip(image_fp = dut_chip_dir, image_fn = dut_chip_fn, ocr_image_dir = ocr_image_dir, degree=180, x=1115, y=785, w=330, h=330, valid_flg=False)
             if ocr_sn not in ocr_info:
                 print ("Chip to be tested have different SN from the tray scanning!")
+                print ("Tray Scan: {ocr_info}, Move to Socket: {ocr_sn}")
                 print (f"file path: {ocr_image_dir}")
                 RTS_debug ("OCR")
 
