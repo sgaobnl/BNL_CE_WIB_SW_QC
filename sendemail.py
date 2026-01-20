015f71e3a9526a40c4be82ca50b7ea6f14e7382f
sgaobnl <gao33.bnl@gmail.com>
2026-01-20 15:31:22 -0500

    Update sendemail.py

--- sendemail.py
+++ sendemail.py
@@ -5,18 +5,9 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from google_sheets_shifter import google_sheets_shifter
 from datetime import date
+from datetime import datetime, time
 
 def sendemail(message, user_email="", subject = "Message from RTS", inform_tech = False, p_shifter=True, s_shifter=False):
-    rts_email_pw_fp = "./rts_email_passcode.txt"
-    if not os.path.isfile(rts_email_pw_fp):
-        print (f"{rts_email_pw_fp} does not exist")
-        print ("PASSCODE only saves at RTS PC locately")
-        print ("Please check, exit anyway")
-        exit()
-    with open(rts_email_pw_fp , 'r', encoding="utf-8") as fp:
-        for line in fp:
-            password = line.strip()
-            break
 
     shifters = google_sheets_shifter()
     today = date.today()
@@ -34,10 +25,43 @@ def sendemail(message, user_email="", subject = "Message from RTS", inform_tech 
         receiver_email = "sgao@bnl.gov;gao33.bnl@gmail.com;" + tech_emails + ";" + user_email
     else:
         receiver_email = "sgao@bnl.gov;gao33.bnl@gmail.com;" + user_email
+
     if p_shifter:
         receiver_email = ";".join([receiver_email, shifters[weekday_iso][0]])
+
+        if 'Congratulations' in message:
+        #if 'only' in message:
+            now = datetime.now().time()
+            is_between = now >= time(17, 0) or now < time(7, 0)
+            is_before_7am = now < time(7, 0)
+            print (is_between, is_before_7am)
+            if is_between:
+                if is_before_7am: #done in early morning
+                    if (weekday_iso <= 5) and (weekday_iso > 1): #Tuesday to Friday
+                        receiver_email = ";".join([receiver_email, shifters[weekday_iso-1][0]]) #notify shifter
+                    elif weekday_iso == 6: #Satursday
+                        receiver_email = ";".join([receiver_email, shifters[1][0]]) #notify monday shifter
+                        receiver_email = ";".join([receiver_email, shifters[5][0]]) #notify monday shifter
+                else: #done prior to 12AM
+                    if (weekday_iso < 5) : #Monday to Friday
+                        receiver_email = ";".join([receiver_email, shifters[weekday_iso+1][0]]) #notify shifter
+                    else:
+                        receiver_email = ";".join([receiver_email, shifters[1][0]]) #notify shifter
+
     if s_shifter:
         receiver_email = ";".join([receiver_email, shifters[weekday_iso][1]])
+
+
+    rts_email_pw_fp = "./rts_email_passcode.txt"
+    if not os.path.isfile(rts_email_pw_fp):
+        print (f"{rts_email_pw_fp} does not exist")
+        print ("PASSCODE only saves at RTS PC locately")
+        print ("Please check, exit anyway")
+        exit()
+    with open(rts_email_pw_fp , 'r', encoding="utf-8") as fp:
+        for line in fp:
+            password = line.strip()
+            break
     
     body = message
     msg = MIMEMultipart()
